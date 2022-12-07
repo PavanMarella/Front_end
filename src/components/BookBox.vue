2ed51cfabea6052018cac10bcfd3fae238c012e4
PavanMarella <101533099+PavanMarella@users.noreply.github.com>
2022-12-06 20:58:37 -0500

    Add files via upload

---
+++ src/components/BookBox.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="bookbox">
+
+
+        
+
+        <div v-for="book in books" :key="book.id" >
+            <BookItem :book="book"/>
+        </div>
+
+    </div>
+    
+
+
+</template>'
+
+<script>
+
+    import BookItem from './BookItem.vue'
+
+
+    export default{
+        name: 'BookBox',
+        components: {
+            BookItem
+        },
+        props:{
+            
+            books: Array,
+            nobook:Array
+        }
+    }
+
+</script>
+
+<style scoped>
+
+.bookbox{
+
+background-color: rgb(238, 190, 131);
+padding: 1em;
+color: gray;
+}
+
+</style>
---
+++ src/components/BookItem.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <div :class="book.product_discontinued? 'red': 'green' ">
+        <h2> {{book.Watch_series}}</h2>
+        <h4> {{book.new_feature[0]}}</h4>
+        <h4> {{book.new_feature[1]}}</h4>
+        <h4> {{book.new_feature[2]}}</h4>
+        <h4> {{book.year_of_release}} </h4>
+        <h4> {{book.OS}} </h4>
+    </div>
+
+</template>
+
+<script>
+
+
+export default{
+    name: 'BookItem',
+    props:{
+        book: Array
+    }
+}
+
+
+</script>
+
+<style scoped>
+
+
+h4{
+    margin: 2px;
+}
+
+div{
+    background-color: #09dfee;
+    color: black;
+    padding: 1em;
+}
+
+.red{
+background-color: rgb(243, 10, 10) !important;
+
+}
+.green{
+    background-color: rgb(15, 214, 82);
+}
+
+
+</style>
